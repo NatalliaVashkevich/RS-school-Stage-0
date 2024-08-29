24c8f5dddf0fc80b60028b917ea40752bff630ff
Natallia <mahatali84@gmail.com>
2024-08-29 12:48:17 +0300

    feat: add burger-menu on js (main)

---
+++ shelter/pages/main/index.js
@@ -0,0 +1,26 @@
+const body = document.body;
+const burger = document.querySelector(".burger");
+const popup = document.querySelector(".popup");
+const filter = document.querySelector(".body-filter");
+
+burger.addEventListener("click", burgerActive);
+function burgerActive() {
+    burger.classList.toggle("active");
+    popup.classList.toggle("open");
+    body.classList.toggle("noscroll");
+    filter.classList.toggle("active");
+}
+
+const links = Array.from(popup.children);
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+    burger.classList.remove("active");
+    popup.classList.remove("open");
+    body.classList.remove("noscroll");
+    filter.classList.remove("active");
+  }
+
+
