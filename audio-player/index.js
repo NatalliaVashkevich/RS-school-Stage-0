a1138901555057d58cd2b3e1a44f7583ecb0f1fe
Natallia <mahatali84@gmail.com>
2024-09-21 00:30:05 +0300

    feat: implement current time

--- audio-player/index.js
+++ audio-player/index.js
@@ -3,8 +3,8 @@ const audio = document.querySelector(".audio-file");
 const playPauseButton = document.querySelector(".play-pause-button");
 const playButton = document.querySelector(".play-button");
 const pauseButton = document.querySelector(".pause-button");
-const currentTime = document.querySelector(".current-time");
-const durationTime = document.querySelector(".duration-time");
+const currentTimeOnDisplay = document.querySelector(".current-time");
+const durationTimeOnDisplay = document.querySelector(".duration-time");
 const nextSong = document.querySelector(".next-song");
 const prevSong = document.querySelector(".prev-song");
 
@@ -24,3 +24,16 @@ playPauseButton.addEventListener("click", () => {
     isPlaying = !isPlaying;
 });
 
+audio.addEventListener("timeupdate", () => {
+    const currentTime = audio.currentTime;
+   
+    const currentMinutes = Math.floor( currentTime / 60);
+    const currentSeconds = Math.floor( currentTime % 60);
+    if (currentTime < 10) {
+        currentTimeOnDisplay.textContent = `0:0${currentSeconds}`;
+    } else if (currentTime < 60) {
+    currentTimeOnDisplay.textContent = `0:${currentSeconds}`;
+} else {
+    currentTimeOnDisplay.textContent = `${currentMinutes}:${currentSeconds}`;
+}
+});
